7646597e49286c5328bd5ef4f43fd81e06908754
pycat <106306334+pycatmc@users.noreply.github.com>
2023-07-15 04:07:51 +0900

    Fixed AutoAnchor's cooldown

--- src/main/java/net/caffeinemc/phosphor/module/modules/combat/AutoAnchorModule.java
+++ src/main/java/net/caffeinemc/phosphor/module/modules/combat/AutoAnchorModule.java
@@ -19,7 +19,7 @@ import org.lwjgl.glfw.GLFW;
 
 public class AutoAnchorModule extends Module {
     public final NumberSetting cooldown = new NumberSetting("Cooldown", this, 0, 0, 20, 1);
-    public final NumberSetting itemSwap = new NumberSetting("Item Swap", this, 0, 0, 8, 1);
+    public final NumberSetting itemSwap = new NumberSetting("Item Swap", this, 1, 1, 9, 1);
     public final BooleanSetting chargeOnly = new BooleanSetting("Charge Only", this, false);
 
     public AutoAnchorModule() {
@@ -34,58 +34,55 @@ public class AutoAnchorModule extends Module {
     public void onEnable() {
         hasAnchored = false;
         hasCharged = false;
-        clock = cooldown.getIValue();
+        clock = 0;
     }
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
-        if (mc.world == null || mc.player == null) return;
+        if (mc.world == null || mc.player == null)
+            return;
 
-        if (GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), 1) != 1)
+        if (GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) != GLFW.GLFW_PRESS)
             return;
 
         if (mc.player.isUsingItem())
             return;
 
         if (hasAnchored) {
-            if (clock != 0) {
-                --clock;
+            if (clock <= cooldown.getIValue()) {
+                clock++;
                 return;
             }
-            clock = cooldown.getIValue();
+
+            clock = 0;
             hasAnchored = false;
         }
 
-        final HitResult cr = mc.crosshairTarget;
+        if (mc.crosshairTarget instanceof BlockHitResult hit) {
+            if (hit.getType() == HitResult.Type.MISS)
+                return;
 
-        if (cr instanceof BlockHitResult) {
-            final BlockHitResult hit = (BlockHitResult) cr;
-            final BlockPos pos = hit.getBlockPos();
+            BlockPos pos = hit.getBlockPos();
+
             if (BlockUtils.isAnchorUncharged(pos)) {
-                if (mc.player.isHolding(Items.GLOWSTONE)) {
-                    final ActionResult actionResult = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, hit);
-                    if (actionResult.isAccepted() && actionResult.shouldSwingHand()) {
-                        mc.player.swingHand(Hand.MAIN_HAND);
-                    }
-                    return;
-                }
-                InvUtils.selectItemFromHotbar(Items.GLOWSTONE);
-                final ActionResult actionResult = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, hit);
+                if (!mc.player.isHolding(Items.GLOWSTONE))
+                    InvUtils.selectItemFromHotbar(Items.GLOWSTONE);
+
+                ActionResult actionResult = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, hit);
                 if (actionResult.isAccepted() && actionResult.shouldSwingHand()) {
                     mc.player.swingHand(Hand.MAIN_HAND);
                 }
             } else if (BlockUtils.isAnchorCharged(pos) && !chargeOnly.isEnabled()) {
-                final PlayerInventory inv = mc.player.getInventory();
-                inv.selectedSlot = itemSwap.getIValue();
+                mc.player.getInventory().selectedSlot = itemSwap.getIValue() - 1;
                 ((ClientPlayerInteractionManagerAccessor) mc.interactionManager).callSyncSelectedSlot();
-                final ActionResult actionResult2 = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, hit);
+
+                ActionResult actionResult2 = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, hit);
                 if (actionResult2.isAccepted() && actionResult2.shouldSwingHand()) {
                     mc.player.swingHand(Hand.MAIN_HAND);
                 }
+
                 hasAnchored = true;
             }
         }
     }
-
-
 }
