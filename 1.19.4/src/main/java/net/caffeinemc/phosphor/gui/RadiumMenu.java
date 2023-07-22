7cef20c6cd0f28ce4a67bf495cea76c9cee52147
NoboKik <segdaproduction@gmail.com>
2023-07-22 13:49:29 +0300

    Color changes

--- 1.19.4/src/main/java/net/caffeinemc/phosphor/gui/RadiumMenu.java
+++ 1.19.4/src/main/java/net/caffeinemc/phosphor/gui/RadiumMenu.java
@@ -2,6 +2,7 @@ package net.caffeinemc.phosphor.gui;
 
 import imgui.ImGui;
 import imgui.flag.ImGuiCol;
+import net.caffeinemc.phosphor.api.font.JColor;
 import net.caffeinemc.phosphor.common.Phosphor;
 import net.caffeinemc.phosphor.module.Module;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -101,6 +102,11 @@ public class RadiumMenu implements Renderable {
             @Override
             public void preRender() {
                 float[][] colors = ImGui.getStyle().getColors();
+
+                float[] color = JColor.getGuiColor().getFloatColor();
+                float[] bColor = JColor.getGuiColor().getFloatColor();
+                float[] dColor = JColor.getGuiColor().getFloatColor();
+
                 colors[ImGuiCol.Text]                   = new float[]{0.80f, 0.84f, 0.96f, 1.00f};
                 colors[ImGuiCol.TextDisabled]           = new float[]{0.42f, 0.44f, 0.53f, 1.00f};
                 colors[ImGuiCol.WindowBg]               = new float[]{0.07f, 0.07f, 0.11f, 1.00f};
@@ -108,9 +114,9 @@ public class RadiumMenu implements Renderable {
                 colors[ImGuiCol.PopupBg]                = new float[]{0.09f, 0.09f, 0.15f, 0.94f};
                 colors[ImGuiCol.Border]                 = new float[]{0.42f, 0.44f, 0.53f, 0.50f};
                 colors[ImGuiCol.BorderShadow]           = new float[]{0.07f, 0.07f, 0.11f, 0.00f};
-                colors[ImGuiCol.FrameBg]                = new float[]{0.65f, 0.24f, 0.33f, 0.54f};
-                colors[ImGuiCol.FrameBgHovered]         = new float[]{0.68f, 0.21f, 0.31f, 0.40f};
-                colors[ImGuiCol.FrameBgActive]          = new float[]{0.68f, 0.21f, 0.31f, 0.67f};
+                colors[ImGuiCol.FrameBg]                = new float[]{color[0], color[1], color[2], 0.54f};
+                colors[ImGuiCol.FrameBgHovered]         = new float[]{color[0], color[1], color[2], 0.40f};
+                colors[ImGuiCol.FrameBgActive]          = new float[]{color[0], color[1], color[2], 0.67f};
                 colors[ImGuiCol.TitleBg]                = new float[]{0.09f, 0.09f, 0.15f, 1.00f};
                 colors[ImGuiCol.TitleBgActive]          = new float[]{0.12f, 0.12f, 0.18f, 1.00f};
                 colors[ImGuiCol.TitleBgCollapsed]       = new float[]{0.09f, 0.09f, 0.15f, 0.75f};
@@ -119,24 +125,24 @@ public class RadiumMenu implements Renderable {
                 colors[ImGuiCol.ScrollbarGrab]          = new float[]{0.25f, 0.27f, 0.35f, 1.00f};
                 colors[ImGuiCol.ScrollbarGrabHovered]   = new float[]{0.32f, 0.34f, 0.43f, 1.00f};
                 colors[ImGuiCol.ScrollbarGrabActive]    = new float[]{0.38f, 0.41f, 0.50f, 1.00f};
-                colors[ImGuiCol.CheckMark]              = new float[]{0.68f, 0.16f, 0.27f, 1.00f};
-                colors[ImGuiCol.SliderGrab]             = new float[]{0.65f, 0.14f, 0.25f, 1.00f};
-                colors[ImGuiCol.SliderGrabActive]       = new float[]{0.70f, 0.15f, 0.27f, 1.00f};
-                colors[ImGuiCol.Button]                 = new float[]{0.65f, 0.24f, 0.33f, 0.59f};
-                colors[ImGuiCol.ButtonHovered]          = new float[]{0.65f, 0.24f, 0.33f, 1.00f};
-                colors[ImGuiCol.ButtonActive]           = new float[]{0.57f, 0.21f, 0.29f, 1.00f};
-                colors[ImGuiCol.Header]                 = new float[]{0.65f, 0.24f, 0.33f, 1.00f};
-                colors[ImGuiCol.HeaderHovered]          = new float[]{0.76f, 0.17f, 0.30f, 1.00f};
-                colors[ImGuiCol.HeaderActive]           = new float[]{0.83f, 0.18f, 0.33f, 1.00f};
+                colors[ImGuiCol.CheckMark]              = new float[]{bColor[0], bColor[1], bColor[2], 1.00f};
+                colors[ImGuiCol.SliderGrab]             = new float[]{color[0], color[1], color[2], 0.9f};
+                colors[ImGuiCol.SliderGrabActive]       = new float[]{color[0], color[1], color[2], 0.95f};
+                colors[ImGuiCol.Button]                 = new float[]{color[0], color[1], color[2], 0.59f};
+                colors[ImGuiCol.ButtonHovered]          = new float[]{color[0], color[1], color[2], 0.9f};
+                colors[ImGuiCol.ButtonActive]           = new float[]{color[0], color[1], color[2], 1.00f};
+                colors[ImGuiCol.Header]                 = new float[]{color[0], color[1], color[2], 0.9f};
+                colors[ImGuiCol.HeaderHovered]          = new float[]{color[0], color[1], color[2], 0.95f};
+                colors[ImGuiCol.HeaderActive]           = new float[]{bColor[0], bColor[1], bColor[2], 1.00f};
                 colors[ImGuiCol.Separator]              = new float[]{0.45f, 0.47f, 0.58f, 0.50f};
                 colors[ImGuiCol.SeparatorHovered]       = new float[]{0.76f, 0.17f, 0.30f, 0.78f};
                 colors[ImGuiCol.SeparatorActive]        = new float[]{0.76f, 0.17f, 0.30f, 1.00f};
-                colors[ImGuiCol.ResizeGrip]             = new float[]{0.65f, 0.24f, 0.33f, 0.59f};
-                colors[ImGuiCol.ResizeGripHovered]      = new float[]{0.76f, 0.17f, 0.30f, 1.00f};
-                colors[ImGuiCol.ResizeGripActive]       = new float[]{0.76f, 0.17f, 0.30f, 1.00f};
-                colors[ImGuiCol.Tab]                    = new float[]{0.60f, 0.22f, 0.30f, 0.86f};
-                colors[ImGuiCol.TabHovered]             = new float[]{0.76f, 0.17f, 0.30f, 0.80f};
-                colors[ImGuiCol.TabActive]              = new float[]{0.62f, 0.14f, 0.25f, 1.00f};
+                colors[ImGuiCol.ResizeGrip]             = new float[]{color[0], color[1], color[2], 0.59f};
+                colors[ImGuiCol.ResizeGripHovered]      = new float[]{bColor[0], bColor[1], bColor[2], 1.00f};
+                colors[ImGuiCol.ResizeGripActive]       = new float[]{color[0], color[1], color[2], 1.00f};
+                colors[ImGuiCol.Tab]                    = new float[]{dColor[0], dColor[1], dColor[2], 0.86f};
+                colors[ImGuiCol.TabHovered]             = new float[]{color[0], color[1], color[2], 0.80f};
+                colors[ImGuiCol.TabActive]              = new float[]{bColor[0], bColor[1], bColor[2], 1.00f};
                 colors[ImGuiCol.TabUnfocused]           = new float[]{0.19f, 0.20f, 0.27f, 1.00f};
                 colors[ImGuiCol.TabUnfocusedActive]     = new float[]{0.51f, 0.12f, 0.20f, 1.00f};
                 colors[ImGuiCol.DockingPreview]         = new float[]{0.26f, 0.59f, 0.98f, 0.70f};
