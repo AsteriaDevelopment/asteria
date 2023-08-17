112c1902f8fe3ffb048c92b0195c45d44ea7f898
pycat <106306334+pycatmc@users.noreply.github.com>
2023-08-18 01:04:17 +0900

    sadf

--- 1.19.4/src/main/java/net/caffeinemc/phosphor/mixin/GameRendererMixin.java
+++ 1.19.4/src/main/java/net/caffeinemc/phosphor/mixin/GameRendererMixin.java
@@ -1,11 +1,16 @@
 package net.caffeinemc.phosphor.mixin;
 
+import net.caffeinemc.phosphor.api.event.events.WorldRenderEvent;
 import net.caffeinemc.phosphor.common.Phosphor;
 import net.caffeinemc.phosphor.module.modules.combat.ReachModule;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
@@ -23,5 +28,10 @@ public class GameRendererMixin {
         if (reachModule.isEnabled()) return reachModule.entityReach.getValue();
 
         return d;
+    }
+
+    @Inject(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;pop()V", shift = At.Shift.BEFORE))
+    private void onPostWorldRender(float tickDelta, long limitTime, MatrixStack matrices, CallbackInfo ci) {
+        Phosphor.EVENTBUS.post(WorldRenderEvent.get(matrices, tickDelta));
     }
 }
--- 1.20.1/src/main/java/net/caffeinemc/phosphor/mixin/EntityMixin.java
+++ 1.20.1/src/main/java/net/caffeinemc/phosphor/mixin/EntityMixin.java
@@ -1,14 +1,18 @@
 package net.caffeinemc.phosphor.mixin;
 
+import net.caffeinemc.phosphor.api.event.events.EntityMoveEvent;
 import net.caffeinemc.phosphor.common.Phosphor;
 import net.caffeinemc.phosphor.gui.AsteriaMenu;
 import net.caffeinemc.phosphor.module.modules.combat.HitboxesModule;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.MovementType;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
@@ -24,5 +28,10 @@ public class EntityMixin {
         if (AsteriaMenu.isClientEnabled() && hitboxes.isEnabled()) {
             cir.setReturnValue(hitboxes.getHitboxSize((Entity) (Object) this));
         }
+    }
+
+    @Inject(method = "move", at = @At("HEAD"))
+    private void onEntityMove(MovementType movementType, Vec3d movement, CallbackInfo ci) {
+        Phosphor.EVENTBUS.post(EntityMoveEvent.get((Entity) (Object) this));
     }
 }
